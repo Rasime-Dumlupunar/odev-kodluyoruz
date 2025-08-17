300e8a5cdcf86435333b0294fff02209b024ead4
Rasime-Dumlupunar <rasimedumlupunar@gmail.com>
2025-08-18 00:38:43 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/odevkodluyoruz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.odevkodluyoruz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // VERİ TİPLERİ
+  int yas = 39;
+  String adim = 'Rasime';
+  double boyum = 1.68;
+  bool evlimiyim = true;
+  dynamic arabaminmarkasi = 'Toyota';
+
+  //CONSOLE
+  print(yas);
+  print(adim);
+  print(boyum);
+  print(evlimiyim);
+  print(arabaminmarkasi);
+
+  int kisiYasi = 25;
+  String kisiAdi= 'Rasime';
+  double kisiBoyu = 1.68;
+  bool evliMidir = true;
+  dynamic arabaminMarkasi = 'Toyota';
+
+  print(kisiYasi);
+  print(kisiAdi);
+  print(kisiBoyu);
+  print(evliMidir);
+  print(arabaminMarkasi);
+
+  int kisi_yasi = 25;
+  String kisi_adi= 'Rasime';
+  double kisi_boyu = 1.68;
+  bool evli_midir = true;
+  dynamic arabamin_markasi = 'Toyota';
+
+  print(kisi_yasi);
+  print(kisi_adi);
+  print(kisi_boyu);
+  print(evli_midir);
+  print(arabamin_markasi);
+
+  int KisiYasi = 25;
+  String KisiAdi= 'Rasime';
+  double KisiBoyu = 1.68;
+  bool EvliMidir = true;
+  dynamic ArabaminMarkasi = 'Toyota';
+
+  print(KisiYasi);
+  print(KisiAdi);
+  print(KisiBoyu);
+  print(EvliMidir);
+  print(ArabaminMarkasi);
+
+  int kendiYasim = 40;
+  String kendiAdim= 'Rasime';
+  double kendiBoyum = 1.68;
+  bool resitMidir = true;
+
+
+  print(kendiYasim);
+  print(kendiAdim);
+  print(kendiBoyum);
+  print(resitMidir);
+
+}
